c7da10417f1d69d3adbe320b190aed604737a0ee
chexing <superchexing@gmail.com>
2017-03-30 14:51:16 +0800

    modify  code

--- user_define_cookie.py
+++ user_define_cookie.py
@@ -3,7 +3,7 @@ class UserDefineCookie:
     def get_xueqiu_cookie():
         # 因为泄漏了自己的个人信息，所以需要填写自己的
         # 通过浏览器开发者工具或者抓包拿到雪球cookie填写
-        cookie = open('cookie_xueqiu.txt', encoding='utf8').read()
+        cookie = open('cookie_xueqiu.txt', encoding='utf8').read().strip()
         print('xueqiu cookie:' + cookie)
         return cookie
 
@@ -11,6 +11,6 @@ class UserDefineCookie:
     def get_guoren_cookie():
         # 因为泄漏了自己的个人信息，所以需要填写自己的
         # 通过浏览器开发者工具或者抓包拿到果仁cookie填写
-        cookie = open('cookie_guoren.txt', encoding='utf8').read()
+        cookie = open('cookie_guoren.txt', encoding='utf8').read().strip()
         print('guoren cookie:' + cookie)
         return cookie
